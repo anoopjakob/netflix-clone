dfecd04c17f57b64ec06711b88252947b1756bba
anoopjakob <anoopjacob9@gmail.com>
2020-11-02 00:41:35 +0530

    finished home page

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import faqsData from '../fixtures/faqs.json';
 
 import { Accordion } from '../components';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer() {
   return (
@@ -13,6 +14,14 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try It Now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
